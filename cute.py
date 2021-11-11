571118b30b91d97a2ba84e975dbcc621702911ce
Bubbles <nghi.nguyen803@gmail.com>
2021-11-11 16:44:59 -0600

    updated cute.py

--- cute.py
+++ cute.py
@@ -1,6 +1,10 @@
 userinput = input("What's your favorite animal?")
 if userinput.lower() == 'sheep':
     print("Omglob, me too! Though they are as dumb as bricks :T")
+elif userinput.lower() == "turtle":
+    print("Cute, slow, but never gives up. A great choice!")
+elif userinput.lower()=="blobfish":
+    print("Er... I guess I can see how it can be cute...?")
 else:
     print("Oh.. That's... neat!")
 
