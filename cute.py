75c94b48f7b42ddc732f9b64b4c1915848607ba3
Bubbles <nghi.nguyen803@gmail.com>
2021-11-11 16:05:50 -0600

    created cute python script

---
+++ cute.py
@@ -0,0 +1,6 @@
+userinput = input("What's your favorite animal?")
+if userinput.lower() == 'sheep':
+    print("Omglob, me too! Though they are as dumb as bricks :T")
+else:
+    print("Oh.. That's... neat!")
+
